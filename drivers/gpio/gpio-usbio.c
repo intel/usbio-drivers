cba4b240a5361db3177a9208c77c9f5846d60c56
Wang, Lifu <lifu.wang@intel.com>
2024-07-23 10:06:18 +0000

    remove project name
    Signed-off-by: Wang, Lifu <lifu.wang@intel.com>

--- drivers/gpio/gpio-usbio.c
+++ drivers/gpio/gpio-usbio.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 /*
- * Intel USBIO-GPIO driver
  *
  * Copyright (c) 2023, Intel Corporation.
  */
--- drivers/mfd/usbio.c
+++ drivers/mfd/usbio.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 /*
- * Intel USBIO-Bridge driver
  *
  * Copyright (c) 2023, Intel Corporation.
  */
